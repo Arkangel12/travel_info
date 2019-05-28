b30cc7923695bbeee4e0870179d43a43f1874c7b
Argel  Bejarano <argel.bc18@gmail.com>
2019-05-27 17:52:19 -0700

    se cambios listview por Google Maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:demo_meetup/src/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(Demo());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.white, // Color for Android
+      statusBarBrightness: Brightness.light // Dark == white status bar -- for IOS.
+  ));
+  runApp(Demo());
+}
 
--- lib/src/screens/countriesScreen.dart
+++ lib/src/screens/countriesScreen.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:demo_meetup/src/api/apiCalls.dart';
 import 'package:demo_meetup/src/models/country.dart';
 import 'package:demo_meetup/src/screens/detailsScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -14,29 +17,37 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ApiCalls apiCalls = ApiCalls();
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _CDMX = CameraPosition(
+    target: LatLng(19.4288169, -99.14961053600166),
+    zoom: 5,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: FutureBuilder<List<Country>>(
         future: apiCalls.getCountries(),
         initialData: [],
         builder: (BuildContext context, AsyncSnapshot<List<Country>> snapshot) {
-          return ListView.builder(
-              shrinkWrap: true,
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                Country country = snapshot.data[index];
-                return ListTile(
-                  onTap: ()=> Navigator.of(context).push(DetailsScreen.route(country)),
-                  trailing: SizedBox(width: 50, height: 50,child: SvgPicture.network(country.flag)),
-                  title: Text(country.name),
-                  subtitle: Text(country.capital),
-                );
-              });
+          return GoogleMap(
+            initialCameraPosition: _CDMX,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            markers: {
+              for(Country country in snapshot.data)
+                if(country.latlng.length > 0)
+                  Marker(
+                  markerId: MarkerId(country.name),
+                  onTap: () => Navigator.of(context).push(DetailsScreen.route(country)),
+                  position: LatLng(country.latlng[0],country.latlng[1],
+                  )
+                ),
+            },
+            myLocationButtonEnabled: false,
+          );
         },
       ),
     );
