64ac142dc43fc2a565e25af9efaefade506f4877
Argel  Bejarano <argel.bc18@gmail.com>
2019-05-30 09:10:22 -0700

    final version

--- lib/src/screens/detailsScreen.dart
+++ lib/src/screens/detailsScreen.dart
@@ -24,109 +24,98 @@ class _DetailsScreenState extends State<DetailsScreen> {
   Widget build(BuildContext context) {
     Country country = widget.country;
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.country.name),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 5),
-              child: SvgPicture.network(country.flag),
-            ),
+    print(country.flag);
+    return Column(
+      children: <Widget>[
+        centerInColumn(
+          value: country.name,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        Container(
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.only(top: 5),
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: SvgPicture.network(
+            country.flag,
+            placeholderBuilder: (BuildContext context) {
+              return CircularProgressIndicator();
+            },
           ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(20),
-              width: width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          width: width,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              centerInColumn(
+                category: 'Capital',
+                value: country.capital,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              centerInColumn(category: 'Nombre Nativo', value: country.nativeName),
+              Text('Moneda: ${country.currencies[0].name} (${country.currencies[0].symbol})'),
+              Text('Población: ${country.population.toString()}'),
+              Text('Codigo de llamada: ${country.callingCodes[0].toString()}'),
+              centerInColumn(category: 'Fronteras', value: ''),
+              Wrap(
+                children: <Widget>[
+                  for (String border in country.borders)
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Text(border),
+                    ),
+                ],
+              ),
+              Text('Lenguaje: ${country.languages[0].name}'),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
-                  centerInColumn(
-                    category: 'Capital',
-                    value: country.capital,
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                  ),
-                  centerInColumn(
-                      category: 'Nombre Nativo', value: country.nativeName),
+                  Text('Traduccir: '),
                   Text(
-                      'Moneda: ${country.currencies[0].name} (${country.currencies[0].symbol})'),
-                  Text('Población: ${country.population.toString()}'),
-                  Text(
-                      'Codigo de llamada: ${country.callingCodes[0].toString()}'),
-                  centerInColumn(category: 'Fronteras', value: ''),
-                  Wrap(
-                    children: <Widget>[
-                      for (String border in country.borders)
-                        Container(
-                          padding: EdgeInsets.all(8),
-                          margin: EdgeInsets.all(5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(30)),
-                            border: Border.all(color: Colors.black),
-                          ),
-                          child: Text(border),
-                        ),
-                    ],
-                  ),
-                  Text('Lenguaje: ${country.languages[0].name}'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Text('Traduccir: '),
-                      Text(
-                        translate,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ],
-                  ),
-                  Container(
-                    width: width,
-                    child: Wrap(
-                      children: <Widget>[
-                        doTranslate(
-                            translation: country.translations.de, to: 'De'),
-                        doTranslate(
-                            translation: country.translations.es, to: 'Es'),
-                        doTranslate(
-                            translation: country.translations.fr, to: 'Fr'),
-                        doTranslate(
-                            translation: country.translations.ja, to: 'Ja'),
-                        doTranslate(
-                            translation: country.translations.it, to: 'It'),
-                        doTranslate(
-                            translation: country.translations.br, to: 'Br'),
-                        doTranslate(
-                            translation: country.translations.pt, to: 'Pt'),
-                        doTranslate(
-                            translation: country.translations.nl, to: 'Nl'),
-                        doTranslate(
-                            translation: country.translations.hr, to: 'Hr'),
-                        doTranslate(
-                            translation: country.translations.fa, to: 'Fa'),
-                      ],
-                    ),
+                    translate,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    overflow: TextOverflow.ellipsis,
                   ),
                 ],
               ),
-            ),
-          )
-        ],
-      ),
+              Container(
+                width: width,
+                child: Wrap(
+                  children: <Widget>[
+                    doTranslate(translation: country.translations.de, to: 'De'),
+                    doTranslate(translation: country.translations.es, to: 'Es'),
+                    doTranslate(translation: country.translations.fr, to: 'Fr'),
+                    doTranslate(translation: country.translations.ja, to: 'Ja'),
+                    doTranslate(translation: country.translations.it, to: 'It'),
+                    doTranslate(translation: country.translations.br, to: 'Br'),
+                    doTranslate(translation: country.translations.pt, to: 'Pt'),
+                    doTranslate(translation: country.translations.nl, to: 'Nl'),
+                    doTranslate(translation: country.translations.hr, to: 'Hr'),
+                    doTranslate(translation: country.translations.fa, to: 'Fa'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 
   Widget centerInColumn({String category, String value, TextStyle style}) {
     return Container(
       alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(vertical: 5),
       child: Text(
-        '$category: $value',
+        category != null ? '$category: $value' : '$value',
         style: style,
         overflow: TextOverflow.ellipsis,
       ),
